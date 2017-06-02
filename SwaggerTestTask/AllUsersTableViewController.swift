1174da11287b66050afaaa4974bb86026b764eb5
rusel95 <ruslanpopesku95@gmail.com>
2017-06-02 21:08:57 +0300

    Add User model

--- SwaggerTestTask/AllUsersTableViewController.swift
+++ SwaggerTestTask/AllUsersTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AllUsersTableViewController: UITableViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- SwaggerTestTask/SwotseApi.swift
+++ SwaggerTestTask/SwotseApi.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import Alamofire
+
+class SwotseApi {
+    
+    static let shared = SwotseApi()
+    private init() { }
+    
+    
+    private let apiSkeletonUrl = "http://api.openweathermap.org/data/2.5/weather"
+    private let apiKeyUrl = "&appid=4a069077d6974db10f255af576ef8baa"
+    private let apiAccuracyUrl = "&type=accurate"
+    private let apiMetricUrl = "&units=metric"
+    
+    func getAllUsers(latitude: Double, longitude: Double, giveData: @escaping (WeatherResponse?) -> Void) -> Void {
+        
+        let locationUrl = "?lat=" + String(latitude) + "&lon=" + String(longitude)
+        let urlForRequest = apiSkeletonUrl + locationUrl + apiAccuracyUrl + apiMetricUrl + apiKeyUrl
+        
+        Alamofire.request(urlForRequest).responseObject { (response: DataResponse<WeatherResponse>) in
+            
+            switch response.result {
+                
+            case .success:
+                let weatherResponse = response.result.value!
+                giveData(weatherResponse)
+                
+            case .failure(let error):
+                print(error.localizedDescription, urlForRequest)
+                giveData(nil)
+            }
+        }
+        
+    }
+    
+}
